84ce873270262023d0661b22c72b49d14b1bf3a8
salmataherfouad71-dev <salma.taherfouad71@gmail.com>
2026-08-19 15:05:06 +0300

    Question02

--- Assignment.10/Program.cs
+++ Assignment.10/Program.cs
@@ -14,6 +14,17 @@
              c)Object Copying:It creates a new independent object(shallow-deap) .
                Reference Copying:It just copies the address (assignment)*/
             #endregion 
+            #region Question02
+            /*
+             a)Shallow Copy:It creates a new object that copies the values of value types
+               and the address of the reference types.
+             b)Deap Copy:It creates a completely new object that copies all the values of the data.
+             c)It just copy the address so the changes in the new object will affect the original 
+               variable except for the string (because it is immutable).
+             d)It copy its value so the changes in the new object will never affect the original variable.
+             e)Deap copy is safer when you want to edit the data and you don't want those
+               changes to accidentally break or alter the original object.*/
+            #endregion
             #endregion
         }
     }
