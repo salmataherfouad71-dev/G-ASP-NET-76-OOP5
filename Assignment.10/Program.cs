b12610efd5657ff59c6a53a99b7f8af61ce61f5e
salmataherfouad71-dev <salma.taherfouad71@gmail.com>
2026-08-19 12:48:01 +0300

    Question01

--- Assignment.10/Program.cs
+++ Assignment.10/Program.cs
@@ -4,6 +4,17 @@
     {
         static void Main(string[] args)
         {
+            #region Part01
+            #region Question01
+            /*
+             a)If it a value type is copies the value,but if it reference type it copies 
+               the reference (address).
+             b)No,If it a value type it just change the value of the object.If it is a
+               reference type it just starts pointing to the assigned object.
+             c)Object Copying:It creates a new independent object(shallow-deap) .
+               Reference Copying:It just copies the address (assignment)*/
+            #endregion 
+            #endregion
         }
     }
 }
